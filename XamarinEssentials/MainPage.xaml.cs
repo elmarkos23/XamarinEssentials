a4659978156af2f7011106be72b6d65e10df484c
Markos Ayala <elmarkos23@gmail.com>
2019-02-07 14:43:07 -0500

    .

--- XamarinEssentials/MainPage.xaml.cs
+++ XamarinEssentials/MainPage.xaml.cs
@@ -13,5 +13,15 @@ namespace XamarinEssentials
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
