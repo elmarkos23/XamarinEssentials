93b259f7413b9da96a19ef4d113e64b6b936b109
Markos Ayala <elmarkos23@gmail.com>
2019-02-11 14:58:08 -0500

    .

---
+++ XamarinEssentials/PageTextToSpeech.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinEssentials
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageTextToSpeech : ContentPage
+	{
+		public PageTextToSpeech()
+		{
+			InitializeComponent();
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync(EntryTexto.Text);
+        }
+    }
+}
